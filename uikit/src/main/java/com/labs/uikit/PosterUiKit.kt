abf2eb38ed0f9ca187ee7f82ad0e676539e9d39f
Rio Arj <ri0arjuna@programmer.net>
2024-03-03 03:01:35 +0700

    add onClick lambda on PosterUiKit.kt

--- uikit/src/main/java/com/labs/uikit/PosterUiKit.kt
+++ uikit/src/main/java/com/labs/uikit/PosterUiKit.kt
@@ -1,31 +1,26 @@
 package com.labs.uikit
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 
 @Composable
-fun PosterUiKit(url: String) {
+fun PosterUiKit(url: String, onClick: () -> Unit) {
     Column {
         AsyncImage(
             modifier = Modifier
@@ -33,8 +28,11 @@ fun PosterUiKit(url: String) {
                 .padding(start = 8.dp, top = 8.dp)
                 .height(260.dp)
                 .clip(RoundedCornerShape(CornerSize(percent = 5)))
-                .background(color = Color.LightGray),
-            model =  ImageRequest.Builder(LocalContext.current)
+                .background(color = Color.LightGray)
+                .clickable {
+                    onClick()
+                },
+            model = ImageRequest.Builder(LocalContext.current)
                 .data(url)
                 .crossfade(true)
                 .build(),
@@ -50,5 +48,5 @@ fun PosterUiKit(url: String) {
 )
 @Composable
 private fun PosterPreview() {
-    PosterUiKit(url = "https://img.freepik.com/free-photo/eiffel-tower-with-bridge-river-seine-paris-france_649448-4968.jpg")
+    PosterUiKit(url = "https://img.freepik.com/free-photo/eiffel-tower-with-bridge-river-seine-paris-france_649448-4968.jpg") {}
 }
