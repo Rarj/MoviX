5b3c0bf26d95b4ef2fd7b26379d82f3bff8bb9c3
Rio Arj <ri0arjuna@programmer.net>
2024-03-03 03:02:07 +0700

    implement compose on View System (XML)

--- app/src/main/java/com/labs/movix/home/HomeViewModel.kt
+++ app/src/main/java/com/labs/movix/home/HomeViewModel.kt
@@ -3,19 +3,19 @@ package com.labs.movix.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.labs.data.Status
 import com.labs.data.ViewState
 import com.labs.data.repository.genre.Genre
 import com.labs.data.repository.genre.GenreRepository
 import com.labs.data.repository.movie.Movie
 import com.labs.data.repository.movie.MovieRepository
-import com.labs.data.repository.movie.Review
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -28,44 +28,21 @@ class HomeViewModel @Inject constructor(
     private val _genres = MutableStateFlow<ViewState<List<Genre>>?>(null)
     val genre get() = _genres.asStateFlow()
 
-    private val _movies =  MutableStateFlow<PagingData<Movie>?>(null)
-    val movies get() = _movies.asStateFlow()
+    var movieFlow: Flow<PagingData<Movie>> = flow { PagingData.empty<Movie>() }
+        private set
 
-    private lateinit var selectedGenre: Genre
+    fun setSelectedGenre(genre: Genre?) = movieRepository.setSelectedGenre(genre)
 
-    fun setSelectedGenre(genre: Genre?) {
-        genre?.let {
-            selectedGenre = it
-        }
-    }
+    fun getSelectedGenre() = movieRepository.getSelectedGenre()
 
-    fun getSelectedGenre() = selectedGenre
-
-    fun getGenre() {
-        viewModelScope.launch {
-            genreRepository.getGenres().collectLatest { genres ->
-                _genres.value = when (genres.status) {
-                    Status.LOADING -> ViewState.loading()
-                    Status.SUCCESS -> ViewState.success(genres.data.orEmpty())
-                    Status.ERROR -> ViewState.error(genres.message.toString())
-                }
-            }
-        }
-    }
-
-    suspend fun getMovie(genre: Genre? = null) {
+    fun getGenres() {
         viewModelScope.launch {
             genreRepository.getGenres().collectLatest { genres ->
                 _genres.value = when (genres.status) {
                     Status.LOADING -> ViewState.loading()
                     Status.SUCCESS -> {
-                        genre?.let {
-                            setSelectedGenre(it)
-                        } ?: setSelectedGenre(genres.data?.firstOrNull())
-                        async {
-                            discoverMovie(listOf(selectedGenre.id.toString()))
-                        }.await()
-
+                        setSelectedGenre(genres.data?.firstOrNull())
+                        async { getMovies() }.await()
                         ViewState.success(genres.data.orEmpty())
                     }
                     Status.ERROR -> ViewState.error(genres.message.toString())
@@ -74,13 +51,9 @@ class HomeViewModel @Inject constructor(
         }
     }
 
-    private suspend fun discoverMovie(genreIds: List<String>) {
+    private suspend fun getMovies() {
         viewModelScope.launch {
-            movieRepository.getDiscoverMovie(genreIds)
-                .cachedIn(this)
-                .collectLatest { movies ->
-                _movies.emit(movies)
-            }
+            movieFlow = movieRepository.getDiscoverMovie()
         }
     }
 
