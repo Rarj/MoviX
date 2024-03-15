e7c8eab13c1f3540fdb410dc739dc656a26c6e19
Rio Arj <ri0arjuna@programmer.net>
2024-03-16 00:42:08 +0700

    implement show/hide NoInternetAccess dialog depending on state

--- app/src/main/java/com/labs/movix/MainActivity.kt
+++ app/src/main/java/com/labs/movix/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.labs.movix
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.labs.data.connectivity.ConnectivityManager
+import com.labs.movix.databinding.ActivityMainBinding
+import com.labs.uikit.NoInternetAccess
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -12,13 +19,35 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var connectivityManager: ConnectivityManager
 
+    private lateinit var binding: ActivityMainBinding
+    private val connectivityViewModel by viewModels<ConnectivityViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.composePreview.apply {
+            setContent {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+                if (connectivityViewModel.connectivityState.collectAsState().value.isShowNoInternetAccessDialog) {
+                    NoInternetAccess {
+                        Log.e("NO_INTERNET_ACCESS: ", "onCreate: onDismiss() invoked")
+                    }
+                }
+            }
+        }
 
         connectivityManager.apply {
-            registerCallback()
+            registerCallback(
+                onShow = {
+                    connectivityViewModel.showNoInternetAccessDialog()
+                },
+                onHide = {
+                    connectivityViewModel.hideNoInternetAccessDialog()
+                })
             registerInstance()
         }
     }
