a8272d775333ff68fcdd6b7c4415866532a549fc
rioarj <ri0arjuna@programmer.net>
2024-07-09 14:02:52 +0700

    add HomeUI.kt on `:home` module to provide compose function

---
+++ home/src/main/java/com/labs/home/HomeUI.kt
@@ -0,0 +1,91 @@
+package com.labs.home
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.paging.compose.LazyPagingItems
+import com.labs.data.BuildConfig
+import com.labs.data.repository.movie.Movie
+import com.labs.uikit.PosterUiKit
+import com.labs.uikit.ToolbarUiKit
+import com.labs.uikit.appearance.ColorSecondaryVariant
+import com.labs.uikit.R as RUiKit
+
+@Composable
+fun HomeUI(
+    modifier: Modifier = Modifier,
+    selectedGenre: String? = null,
+    onSearchClicked: () -> Unit,
+    onFilterClicked: () -> Unit,
+    onItemClicked: (movieId: Int?) -> Unit,
+    lazyPagingItems: LazyPagingItems<Movie>,
+) {
+    ConstraintLayout(modifier = modifier) {
+        val (toolbar, genre, movies) = createRefs()
+
+        ToolbarUiKit(
+            modifier = Modifier.constrainAs(toolbar) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            onSearchClicked = { onSearchClicked.invoke() },
+            onFilterClicked = { onFilterClicked.invoke() },
+        )
+
+        Text(
+            modifier = Modifier
+                .constrainAs(genre) {
+                    top.linkTo(toolbar.bottom)
+                    start.linkTo(parent.start)
+                }
+                .padding(top = 16.dp, end = 16.dp, start = 16.dp),
+            text = selectedGenre.orEmpty(),
+            color = ColorSecondaryVariant,
+            fontSize = 18.sp,
+            fontFamily = FontFamily(Font(RUiKit.font.sono_medium)),
+        )
+
+        MoviesUI(
+            modifier = Modifier.constrainAs(movies) {
+                top.linkTo(genre.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            lazyPagingItems = lazyPagingItems,
+        ) { movieId ->
+            onItemClicked.invoke(movieId)
+        }
+    }
+}
+
+@Composable
+private fun MoviesUI(
+    modifier: Modifier,
+    lazyPagingItems: LazyPagingItems<Movie>,
+    onItemClick: (movieId: Int?) -> Unit,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(count = 2),
+        modifier = modifier.padding(start = 8.dp, end = 8.dp, top = 16.dp)
+    ) {
+        items(lazyPagingItems.itemCount) { index ->
+            lazyPagingItems[index]?.posterPath?.let { url ->
+                PosterUiKit(url = buildString {
+                    append(BuildConfig.IMAGE_BASE_URL)
+                    append(url)
+                }) {
+                    onItemClick.invoke(lazyPagingItems[index]?.id)
+                }
+            }
+        }
+    }
+}
