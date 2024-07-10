29c1f9e3dd3d31ed07425654565c0e03c8448878
rioarj <ri0arjuna@programmer.net>
2024-07-10 15:19:58 +0700

    add discover movie service to HomeService.kt

--- home/api/src/main/java/com/labs/home/api/HomeService.kt
+++ home/api/src/main/java/com/labs/home/api/HomeService.kt
@@ -1,11 +1,20 @@
 package com.labs.home.api
 
-import com.labs.home.api.response.GenreResponse
+import com.labs.home.api.response.discover.Movie
+import com.labs.home.api.response.genre.GenreResponse
+import com.labs.network.shared.NetworkResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface HomeService {
 
     @GET("genre/movie/list")
     suspend fun getGenre(): GenreResponse
+
+    @GET("discover/movie")
+    suspend fun getDiscoverMovie(
+        @Query("with_genres") genreIds: String,
+        @Query("page") page: Int,
+    ): NetworkResponse<Movie>
 
 }
