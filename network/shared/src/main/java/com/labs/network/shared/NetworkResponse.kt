531f0b2920a488462a3a4d8b664f42481eb7143b
rioarj <ri0arjuna@programmer.net>
2024-07-10 15:19:30 +0700

    add NetworkResponse.kt to `:network:shared`

---
+++ network/shared/src/main/java/com/labs/network/shared/NetworkResponse.kt
@@ -0,0 +1,10 @@
+package com.labs.network.shared
+
+import com.google.gson.annotations.SerializedName
+
+data class NetworkResponse<T : Any>(
+    val page: Int?,
+    @SerializedName("total_pages")
+    val totalPages: Int?,
+    val results: List<T>,
+)
