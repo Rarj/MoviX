9a5325ea6f621638f107bb959e479fe1b0dc0cf0
Rio Arj <ri0arjuna@programmer.net>
2024-07-16 12:15:01 +0700

    use compose navigation on Home page

--- app/src/main/java/com/labs/movix/MainActivity.kt
+++ app/src/main/java/com/labs/movix/MainActivity.kt
@@ -3,14 +3,15 @@ package com.labs.movix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.labs.home.ui.HomeUI
-import com.labs.navigation.home.controller.Navigation
+import com.labs.navigation.home.controller.HOME_ROUTE
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -23,15 +24,15 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
 
                 NavHost(
-                    navController, startDestination = "homey"
+                    navController, startDestination = HOME_ROUTE
                 ) {
-                    composable(route = "homey") {
+                    composable(route = HOME_ROUTE) {
                         HomeUI(
+                            modifier = Modifier.fillMaxSize(),
                             onSearchClicked = { println("CLICKED_EVENT: SEARCH") },
                             onFilterClicked = { println("CLICKED_EVENT: FILTER") },
-                        ) {
-
-                        }
+                            onItemClicked = { movieId -> println("CLICKED_EVENT: MOVIE_CLICKED : $movieId") },
+                        )
                     }
                 }
             }
--- navigation/home/controller/src/main/java/com/labs/navigation/home/controller/Navigation.kt
+++ navigation/home/controller/src/main/java/com/labs/navigation/home/controller/Navigation.kt
@@ -2,6 +2,7 @@ package com.labs.navigation.home.controller
 
 import androidx.navigation.NavHostController
 
+const val HOME_ROUTE = "homey"
 const val DETAIL_MOVIE_ROUTE = "detail-movie"
 
 interface Navigation {
