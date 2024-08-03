46562cdd71e2f5171be7f95d1d6d02842618ae62
Rio Arj <ri0arjuna@programmer.net>
2024-08-03 17:17:55 +0700

    layouting Detail Movie page

--- detail/ui/src/main/java/com/labs/detail/ui/DetailMovieUI.kt
+++ detail/ui/src/main/java/com/labs/detail/ui/DetailMovieUI.kt
@@ -1,12 +1,180 @@
 package com.labs.detail.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.labs.uikit.R
+import com.labs.uikit.appearance.ColorPrimary
+import com.labs.uikit.appearance.ColorSecondaryVariant
+import com.labs.uikit.appearance.ColorStar
+import com.labs.uikit.appearance.ColorWhite
+import com.labs.uikit.R as RUiKit
 
 @Composable
 fun DetailMovieUI(
     modifier: Modifier = Modifier,
     movieId: String,
 ) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = ColorPrimary)
+    ) {
+        val (topBar, content, buttonSeeReview) = createRefs()
+        createVerticalChain(
+            topBar,
+            content,
+            buttonSeeReview,
+            chainStyle = ChainStyle.Packed(bias = 0f)
+        )
 
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                }
+                .wrapContentSize()
+                .padding(top = 32.dp, end = 8.dp)
+        ) {
+            IconButton(
+                onClick = {
+                    // TODO: back to the last page
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    tint = ColorWhite,
+                    contentDescription = null,
+                )
+            }
+
+            Text(
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 8.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Start,
+                maxLines = 3,
+                text = "Avenger",
+                color = ColorWhite,
+                fontSize = 28.sp,
+                fontFamily = FontFamily(Font(resId = R.font.sono_extrabold))
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .constrainAs(content) {
+                    top.linkTo(topBar.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(buttonSeeReview.top)
+                    height = Dimension.fillToConstraints
+                }
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp)
+                    .height(260.dp)
+                    .clip(RoundedCornerShape(CornerSize(percent = 5)))
+                    .background(color = Color.LightGray),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("url")
+                    .crossfade(true)
+                    .build(),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = "Poster Movie"
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .wrapContentSize(),
+                    tint = ColorStar,
+                    painter = painterResource(id = RUiKit.drawable.ic_star),
+                    contentDescription = "Rating Icon - Star"
+                )
+
+                Text(
+                    modifier = Modifier
+                        .wrapContentSize(),
+                    text = "4.5/10",
+                    maxLines = 1,
+                    color = ColorWhite,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(resId = R.font.sono_bold))
+                )
+            }
+
+            Text(
+                modifier = Modifier
+                    .padding(16.dp),
+                text = "Long Overview",
+                color = ColorWhite,
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                fontFamily = FontFamily(Font(R.font.sono_light))
+            )
+        }
+
+        Button(
+            modifier = Modifier
+                .constrainAs(buttonSeeReview) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = ColorSecondaryVariant
+            )
+        ) {
+            Text(text = "See Review")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, backgroundColor = 0xFF2C394B)
+@Composable
+private fun DetailMovieUIPreview() {
+    DetailMovieUI(movieId = "movieId")
 }
