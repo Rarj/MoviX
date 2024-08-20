ee207cae52b87d730b045bdfab4c28fcf4554c93
rioarj <ri0arjuna@programmer.net>
2024-08-20 10:51:26 +0700

    use color scheme on MaterialTheme: Detail Page

--- app/src/main/java/com/labs/movix/appearance/Theme.kt
+++ app/src/main/java/com/labs/movix/appearance/Theme.kt
@@ -1,5 +1,6 @@
 package com.labs.movix.appearance
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -100,6 +105,15 @@ fun MovixTheme(
 
         darkTheme -> darkScheme
         else -> lightScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val activity = view.context as Activity
+            activity.window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
