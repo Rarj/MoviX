999277df74863560e670163ba67d94f220df3f4e
Rio Arj <23600466+Rarj@users.noreply.github.com>
2024-08-23 14:56:09 +0700

    enhancement(home): Add Title and Release Date on Home Page (#28)

--- home/impl/src/main/java/com/arj/home/impl/discover/mapper/DiscoverMovieMapper.kt
+++ home/impl/src/main/java/com/arj/home/impl/discover/mapper/DiscoverMovieMapper.kt
@@ -1,6 +1,9 @@
 package com.arj.home.impl.discover.mapper
 
 import com.arj.home.api.response.discover.Movie
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 fun Movie.toDiscoverMovie() = DiscoverMovie(
     id = id,
@@ -9,4 +12,10 @@ fun Movie.toDiscoverMovie() = DiscoverMovie(
     title = title,
     overview = overview,
     rating = rating,
+    releaseDate = releaseDate.getDateFormatted(),
 )
+
+fun String.getDateFormatted(): String {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(this)
+    return dateFormat?.let { SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(it) } ?: ""
+}
