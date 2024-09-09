2998ba1a161db203f3104690d0570fbbbbe92113
Rio Arj <ri0arjuna@programmer.net>
2024-09-09 15:19:04 +0700

    initializing Firebase Crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlinkapt) apply false
+    alias(libs.plugins.googleService) apply false
 }
