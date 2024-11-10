8eab5fd5b4fcf7314c99cd3489d1627ca4c23ca9
Rio Arj <ri0arjuna@programmer.net>
2024-11-10 20:19:50 +0700

    chore(review): Adding `:review:domain` module 📦

---
+++ review/domain/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlinkapt)
+}
+
+android {
+    namespace = "com.arj.review.domain"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 23
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":network:shared"))
+
+    implementation(project(":review:impl"))
+    implementation(project(":review:api"))
+    implementation(project(":review:domain"))
+
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
+}
+
+kapt {
+    correctErrorTypes = true
+}
