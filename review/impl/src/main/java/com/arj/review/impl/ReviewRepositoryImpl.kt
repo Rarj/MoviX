9bb1ea1306cb4dd733b631ec16c86242f8b90666
Rio Arj <ri0arjuna@programmer.net>
2024-11-16 11:00:49 +0700

    chore(review): Adding `dispatcher` to make testing easier

--- review/impl/src/main/java/com/arj/review/impl/ReviewRepositoryImpl.kt
+++ review/impl/src/main/java/com/arj/review/impl/ReviewRepositoryImpl.kt
@@ -1,19 +1,21 @@
 package com.arj.review.impl
 
 import androidx.paging.PagingData
+import com.arj.network.shared.MovixNetworkResult
 import com.arj.network.shared.NetworkResponse
 import com.arj.network.shared.createPager
 import com.arj.review.api.ReviewService
 import com.arj.review.domain.ReviewRepository
 import com.arj.review.domain.model.ReviewModel
 import com.arj.review.domain.toReview
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class ReviewRepositoryImpl @Inject constructor(
-    private val service: ReviewService
+    private val service: ReviewService,
+    private val dispatcher: CoroutineDispatcher,
 ) : ReviewRepository {
 
     override suspend fun getReview(movieId: String): Flow<PagingData<ReviewModel>> {
@@ -21,11 +23,12 @@ class ReviewRepositoryImpl @Inject constructor(
             val response = service.getReviews(movieId, page)
             val results = response.results.map { result -> result.toReview() }
 
-            NetworkResponse(
+            val result = NetworkResponse(
                 page = response.page,
                 totalPages = response.totalPages,
                 results = results,
             )
-        }.flow.flowOn(Dispatchers.IO)
+            MovixNetworkResult.Success(result)
+        }.flow.flowOn(dispatcher)
     }
 }
