a4630ce41e4ca0492e73c96a8949d1c5cff7998e
Rio Arj <ri0arjuna@programmer.net>
2024-11-16 10:24:09 +0700

    chore(network): Adding `safeCall()` to prevent apps crash

---
+++ network/shared/src/main/java/com/arj/network/shared/MovixNetwork.kt
@@ -0,0 +1,45 @@
+package com.arj.network.shared
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.HttpException
+import java.net.UnknownHostException
+
+suspend fun <T> safeCall(
+    dispatcher: CoroutineDispatcher,
+    apiService: suspend () -> T
+): Flow<MovixNetworkResult<T>> {
+    return flow {
+        emit(MovixNetworkResult.Loading())
+        emit(MovixNetworkResult.Success(apiService.invoke()))
+    }.catch { cause: Throwable ->
+        when (cause) {
+            is UnknownHostException -> {
+                emit(
+                    MovixNetworkResult.Failed(
+                        message = "No internet connection",
+                        code = 503,
+                    )
+                )
+            }
+
+            is HttpException -> {
+                when (cause.code()) {
+                    401 -> emit(
+                        MovixNetworkResult.Failed(
+                            code = cause.code(),
+                            message = "Unauthorized"
+                        )
+                    )
+
+                    else -> emit(MovixNetworkResult.Failed(message = "Unknown Error"))
+                }
+            }
+
+            else -> emit(MovixNetworkResult.Failed(message = cause.message))
+        }
+    }.flowOn(dispatcher)
+}
