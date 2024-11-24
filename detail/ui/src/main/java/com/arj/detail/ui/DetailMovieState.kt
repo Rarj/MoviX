130be683a800ea171550d97d0bd0dcd0c9029c38
Rio Arj <ri0arjuna@programmer.net>
2024-11-24 08:51:48 +0700

    chore(detail): Implementing network response using state on the Detail Movie page

--- detail/ui/src/main/java/com/arj/detail/ui/DetailMovieState.kt
+++ detail/ui/src/main/java/com/arj/detail/ui/DetailMovieState.kt
@@ -4,6 +4,26 @@ import androidx.compose.runtime.Immutable
 import com.arj.detail.domain.mapper.Cast
 import com.arj.detail.domain.mapper.Crew
 
+sealed interface DetailMovieUIState {
+    data object Init : DetailMovieUIState
+    data object Loading : DetailMovieUIState
+    data class Success(val data: DetailMovieState) : DetailMovieUIState
+    data class Error(val message: String? = null) : DetailMovieUIState
+}
+
+sealed interface CreditsMovieUIState {
+    data object Init : CreditsMovieUIState
+    data object Loading : CreditsMovieUIState
+    data class Success(val data: CreditsMovieState) : CreditsMovieUIState
+    data class Error(val message: String? = null) : CreditsMovieUIState
+}
+
+@Immutable
+data class CreditsMovieState(
+    val casts: List<Cast> = emptyList(),
+    val crews: List<Crew> = emptyList(),
+)
+
 @Immutable
 data class DetailMovieState(
     val title: String = "",
@@ -13,6 +33,4 @@ data class DetailMovieState(
     val releaseDate: String = "",
     val status: String = "",
     val genres: List<String> = emptyList(),
-    val casts: List<Cast> = emptyList(),
-    val crews: List<Crew> = emptyList(),
 )
--- detail/ui/src/main/java/com/arj/detail/ui/DetailMovieViewModel.kt
+++ detail/ui/src/main/java/com/arj/detail/ui/DetailMovieViewModel.kt
@@ -2,14 +2,16 @@ package com.arj.detail.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.arj.detail.domain.mapper.CreditsMovie
+import com.arj.detail.domain.mapper.DetailMovie
 import com.arj.detail.domain.usecase.CreditUseCase
 import com.arj.detail.domain.usecase.DetailMovieUseCase
+import com.arj.network.state.MovixNetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.math.roundToInt
@@ -20,8 +22,11 @@ class DetailMovieViewModel @Inject constructor(
     private val creditsCreditUseCase: CreditUseCase,
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow(DetailMovieState())
+    private val _state = MutableStateFlow<DetailMovieUIState>(DetailMovieUIState.Init)
     val state get() = _state.asStateFlow()
+
+    private val _creditsState = MutableStateFlow<CreditsMovieUIState>(CreditsMovieUIState.Init)
+    val creditsState get() = _creditsState.asStateFlow()
 
     fun getDetailMovie(movieId: String) {
         viewModelScope.launch {
@@ -30,28 +35,51 @@ class DetailMovieViewModel @Inject constructor(
 
             async {
                 detailMovieFlow.collectLatest { response ->
-                    _state.update {
-                        it.copy(
-                            title = response.title,
-                            posterPath = response.posterPath,
-                            rating = getRating(response.rating),
-                            overview = response.overview,
-                            releaseDate = response.releaseDate,
-                            status = response.status,
-                            genres = response.genres.orEmpty().map { genre -> genre.name },
-                        )
-                    }
+                    detailMovieResponseHandler(response)
                 }
                 creditsFlow.collectLatest { response ->
-                    _state.update {
-                        it.copy(
-                            casts = response.casts.orEmpty(),
-                            crews = response.crews.orEmpty(),
-                        )
-                    }
+                    creditsMovieResponseHandler(response)
                 }
             }.await()
         }
+    }
+
+    private fun creditsMovieResponseHandler(response: MovixNetworkResult<CreditsMovie>) {
+        val uiState = when (response) {
+            is MovixNetworkResult.Loading -> CreditsMovieUIState.Loading
+            is MovixNetworkResult.Success -> CreditsMovieUIState.Success(
+                CreditsMovieState(
+                    casts = response.value.casts.orEmpty(),
+                    crews = response.value.crews.orEmpty(),
+                )
+            )
+            is MovixNetworkResult.Failed -> CreditsMovieUIState.Error(response.message)
+        }
+        _creditsState.value = uiState
+    }
+
+    private fun detailMovieResponseHandler(response: MovixNetworkResult<DetailMovie>) {
+        val uiState = when (response) {
+            is MovixNetworkResult.Loading -> DetailMovieUIState.Loading
+            is MovixNetworkResult.Success -> DetailMovieUIState.Success(
+                DetailMovieState(
+                    title = response.value.title,
+                    posterPath = response.value.posterPath,
+                    rating = getRating(response.value.rating),
+                    overview = response.value.overview,
+                    releaseDate = response.value.releaseDate,
+                    status = response.value.status,
+                    genres = response.value.genres.orEmpty().map { genre -> genre.name },
+                )
+            )
+
+            is MovixNetworkResult.Failed -> DetailMovieUIState.Error(response.message)
+        }
+        setDetailMovieUiState(uiState)
+    }
+
+    private fun setDetailMovieUiState(state: DetailMovieUIState) {
+        _state.value = state
     }
 
     private fun getRating(rating: Double? = 0.0) = buildString {
