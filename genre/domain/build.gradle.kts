f424dc9085e9ec7de6af3701b89c5f0567bd1ddb
Rio Arj <ri0arjuna@programmer.net>
2024-11-27 16:59:21 +0700

    add hilt dependency on `build.gradle.kts`

--- genre/domain/build.gradle.kts
+++ genre/domain/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlinkapt)
 }
 
 android {
@@ -36,4 +38,10 @@ dependencies {
     implementation(project(":genre:api"))
     implementation(project(":network:shared"))
     implementation(project(":network:state"))
+    kapt(libs.dagger.hilt.compiler)
+    implementation(libs.dagger.hilt)
+}
+
+kapt {
+    correctErrorTypes = true
 }
