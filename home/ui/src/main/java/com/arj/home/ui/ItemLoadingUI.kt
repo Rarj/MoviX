0eed502a7ec07a7330353cee1dd68b24e006ca7e
Rio Arj <ri0arjuna@programmer.net>
2024-12-01 12:16:57 +0700

    chore(home): Creating ItemLoadingUI.kt to provide shimmering loader when user has reached the bottom of the page

---
+++ home/ui/src/main/java/com/arj/home/ui/ItemLoadingUI.kt
@@ -0,0 +1,58 @@
+package com.arj.home.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.arj.uikit.shimmerLoadingUI
+
+@Composable
+internal fun ItemLoading(modifier: Modifier = Modifier) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = (configuration.screenWidthDp.dp / 2) - 16.dp
+    Column {
+        Spacer(
+            modifier = modifier
+                .padding(start = 4.dp, top = 8.dp, end = 4.dp)
+                .width(screenWidth)
+                .height(300.dp)
+                .clip(RoundedCornerShape(CornerSize(percent = 5)))
+                .background(shimmerLoadingUI())
+        )
+
+        Spacer(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .padding(horizontal = 4.dp)
+                .width(screenWidth)
+                .height(24.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(shimmerLoadingUI())
+        )
+
+        Spacer(
+            modifier = Modifier
+                .padding(start = 4.dp, end = 4.dp, bottom = 12.dp)
+                .width(screenWidth)
+                .height(24.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(shimmerLoadingUI())
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ItemLoadingPreview() {
+    ItemLoading()
+}
