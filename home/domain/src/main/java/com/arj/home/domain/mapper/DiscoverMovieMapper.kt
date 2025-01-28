3ee8d31722a7fe53a21e65260a1e8b7480741db9
rioarj <rio.arjuna@digits.id>
2025-01-28 22:22:09 +0700

    refactor(home): Return Release Date as an empty if release status is RELEASED

--- home/domain/src/main/java/com/arj/home/domain/mapper/DiscoverMovieMapper.kt
+++ home/domain/src/main/java/com/arj/home/domain/mapper/DiscoverMovieMapper.kt
@@ -1,21 +1,28 @@
 package com.arj.home.domain.mapper
 
 import com.arj.common.utils.DateUtils
+import com.arj.common.utils.ReleaseStatus
 import com.arj.home.api.response.discover.Movie
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 
-fun Movie.toDiscoverMovie() = DiscoverMovie(
-    id = id,
-    posterPath = posterPath,
-    genreIds = genreIds,
-    title = title,
-    overview = overview,
-    rating = rating,
-    releaseDate = releaseDate.getDateFormatted(),
-    releaseStatus = DateUtils.getReleaseStatus(releaseDate).status(),
-    releaseStatusBackground = DateUtils.getReleaseStatus(releaseDate).backgroundColor(),
-)
+fun Movie.toDiscoverMovie(): DiscoverMovie {
+    val releaseStatus = DateUtils.getReleaseStatus(releaseDate).status()
+    val date = releaseDate.getDateFormatted().takeIf {
+        releaseStatus != ReleaseStatus.RELEASED.status()
+    }.orEmpty()
+    return DiscoverMovie(
+        id = id,
+        posterPath = posterPath,
+        genreIds = genreIds,
+        title = title,
+        overview = overview,
+        rating = rating,
+        releaseDate = date,
+        releaseStatus = DateUtils.getReleaseStatus(releaseDate).status(),
+        releaseStatusBackground = DateUtils.getReleaseStatus(releaseDate).backgroundColor(),
+    )
+}
 
 fun String.getDateFormatted(): String {
     if (this.isEmpty()) return ""
