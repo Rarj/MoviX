4000c09202cfc4e93aaf526b7d18c2f6091b1b48
rioarj <rio.arjuna@digits.id>
2025-03-02 14:06:13 +0700

    feature(now playing): Creating new `:navigation:filter:controller` module

---
+++ genre/ui/src/main/java/com/arj/genre/state/FilterViewModel.kt
@@ -0,0 +1,14 @@
+package com.arj.genre.state
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FilterViewModel @Inject constructor(): ViewModel() {
+
+    fun showFilterVisibility() {
+
+    }
+
+}
