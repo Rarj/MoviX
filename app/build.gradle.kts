7c35d62f5910a9be19e02d4b4b3b5fa84ca63dde
Rio Arj <developer.arj@gmail.com>
2025-07-27 02:31:31 -0500

    refactor(app): Renaming debug app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-DEBUG"
-            resValue(type = "string", name = "app_name", value = "Cinepeek Debug")
+            resValue(type = "string", name = "app_name", value = "Cine Peek - Debug")
         }
         release {
             isDebuggable = false
