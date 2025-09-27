17942e52dd7713ffacf6cbe91a9d69c00158b2f1
Rio Arj <developer.arj@gmail.com>
2025-09-27 11:10:49 +0700

    feat(login): Creating LoginModule.kt

---
+++ login/di/src/main/java/com/cinepeek/login/di/LoginModule.kt
@@ -0,0 +1,35 @@
+package com.cinepeek.login.di
+
+import com.cinepeek.login.api.LoginService
+import com.cinepeek.login.domain.LoginRepository
+import com.cinepeek.login.domain.usecase.LoginUseCase
+import com.cinepeek.login.impl.LoginRepositoryImpl
+import com.cinepeek.network.state.IoDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+class LoginModule {
+
+	@Provides
+	fun provideLoginRepository(
+		service: LoginService,
+		@IoDispatcher dispatcher: CoroutineDispatcher,
+	): LoginRepository {
+		return LoginRepositoryImpl(service, dispatcher)
+	}
+
+	@Provides
+	fun provideLoginService(retrofit: Retrofit): LoginService {
+		return retrofit.create(LoginService::class.java)
+	}
+
+	@Provides
+	fun provideLoginUseCase(loginRepository: LoginRepository) = LoginUseCase(loginRepository)
+
+}
